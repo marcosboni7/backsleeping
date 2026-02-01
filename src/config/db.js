3ba36c23d47c52e56d93825ec5c5d78a191dbb72
linkah <developer@linkah.com.br>
2026-02-01 20:59:19 -0300

    Fix: Conexão final com Neon

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',      // Usuário padrão do Postgres (confirme se é o seu)
-    password: 'admin',     // Sua senha
-    database: 'sleeping',  // Seu banco
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // Necessário para conexões seguras no Render
   },
 });
 
